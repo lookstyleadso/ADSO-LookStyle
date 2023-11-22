4df886ee2af92ce7a432ea12365150af02b9d339
Braian López <pformativoadso@gmail.co>
2023-11-22 14:00:45 -0500

    style: Changes are updated

--- Frontend-LookStyle/app/barbershops/SearchList.tsx
+++ Frontend-LookStyle/app/barbershops/SearchList.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { Skeleton } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 
 type BarbershopProps = {
@@ -52,27 +53,38 @@ const SearchList = () => {
               <p className="text-sm leading-6 text-gray-900">
                 {barbershop.location}
               </p>
-              {/* {barbershop.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen{" "}
-                  <time dateTime={barbershop.lastSeenDateTime}>
-                    {barbershop.lastSeen}
-                  </time>
-                </p>
-              ) : (
+              {barbershop.state === "Activo" || barbershop.state === "Abierto" ? (
                 <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                  <div className="flex-none rounded-full p-1 bg-emerald-500/20">
                     <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
                   </div>
                   <p className="text-xs leading-5 text-gray-500">
                     {barbershop.state}
                   </p>
                 </div>
-              )} */}
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full p-1 bg-red-900">
+                    <div className="h-1.5 w-1.5 rounded-full bg-red-600" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    {barbershop.state}
+                  </p>
+                </div>
+              )}
             </div>
           </li>
         ))}
       </ul>
+      <div className="max-w-[300px] w-full flex items-center gap-3">
+        <div>
+          <Skeleton className="flex rounded-full w-12 h-12 bg-red-400" />
+        </div>
+        <div className="w-full flex flex-col gap-2">
+          <Skeleton className="h-3 w-3/5 rounded-lg bg-red-400" />
+          <Skeleton className="h-3 w-4/5 rounded-lg bg-red-400" />
+        </div>
+      </div>
     </div>
   );
 };
