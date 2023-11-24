c67fc997e7d8b5af895ef53ac4887f7700bd5f47
Braian López <braianortiz1603@gmail.com>
2023-11-23 23:58:33 -0500

    feat: The search function is implemented

--- Frontend-LookStyle/app/barbershops/List.tsx
+++ Frontend-LookStyle/app/barbershops/List.tsx
@@ -1,9 +1,8 @@
 "use client";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import SearchInput from "@/components/SearchInput";
 import ButtonTwo from "@/components/ButtonTwo";
+import Image from "next/image";
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
 
 type BarbershopProps = {
   id?: number;
@@ -16,8 +15,9 @@ type BarbershopProps = {
 };
 
 const List = () => {
-  const [barbershop, setBarbershop] = useState<BarbershopProps[]>([]);
-  const [Loading, SetLoading] = useState("Cargando");
+  const [data_barbershop, setBarbershop] = useState<BarbershopProps[]>([]);
+  const [Loading, setLoading] = useState("Cargando");
+  const [search, setSearch] = useState("");
   const url = "https://adso-lookstyle.onrender.com/api/v1/barbershops";
 
   useEffect(() => {
@@ -25,30 +25,56 @@ const List = () => {
       const response = await fetch(url);
       const data = await response.json();
       if (response.status == 200) {
-        SetLoading("");
+        setLoading("");
       }
-      console.log(data);
       setBarbershop(data.data);
     };
     fetchData();
   }, []);
+
+  //Funcion de busqueda
+  const searcher = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  //Metodo de filtrado
+  const results = !search
+    ? data_barbershop
+    : data_barbershop.filter((dataname) =>
+        dataname.barbershop_name
+          .toLowerCase()
+          .includes(search.toLocaleLowerCase())
+      );
+
   return (
     <div className="max-container padding-container pt-32">
-      <SearchInput />
+      <div className="sm:col-span-2">
+        <div className="mt-2.5 flex gap-2 justify-center">
+          <input
+            value={search}
+            onChange={searcher}
+            type="text"
+            placeholder="Buscar"
+            className="block w-3/6 rounded-md border-0 px-3 py-2.5 focus:outline-none focus:border-secundarycolor-sc  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-secundarycolor-sc sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
       <ul role="list" className="divide-y my-20 px-5 divide-gray-300">
-        {barbershop.map((barbershop) => (
+        {results.map((barbershop) => (
           <li key={barbershop.id} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
-              {barbershop.profile_photo.map((photo, index) => (
-                <Image
-                  key={index}
-                  src={photo}
-                  alt="Perfil"
-                  width={500}
-                  height={500}
-                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                />
-              ))}
+              {barbershop.profile_photo &&
+                barbershop.profile_photo.map((photo, index) => (
+                  <Image
+                    key={index}
+                    src={photo}
+                    alt="Perfil"
+                    width={500}
+                    height={500}
+                    className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  />
+                ))}
 
               <div className="min-w-0 flex-auto">
                 <p className="bold-16 font-semibold leading-6 text-gray-900">
@@ -112,5 +138,4 @@ const List = () => {
     </div>
   );
 };
-
 export default List;
