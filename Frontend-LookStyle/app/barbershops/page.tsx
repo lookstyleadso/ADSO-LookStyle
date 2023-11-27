ab1e5b9ff6abaab252bb7243508a31e9015950b4
Braian López <pformativoadso@gmail.co>
2023-11-27 13:35:14 -0500

    refactor: Chnages

--- Frontend-LookStyle/app/barbershops/page.tsx
+++ Frontend-LookStyle/app/barbershops/page.tsx
@@ -1,13 +1,11 @@
 import NavBar from "@/components/NavBar";
 import List from "./List";
-import Footer from "@/components/Footer";
 
 export default function BarbershopsPage() {
   return (
     <>
       <NavBar />
       <List />
-      <Footer />
     </>
   );
 }
