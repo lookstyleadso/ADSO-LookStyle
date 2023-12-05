c4e2ad3f4c73c47e3cbb862c1568a54133f29bf9
Braian López <braianortiz1603@gmail.com>
2023-12-04 22:38:35 -0500

    fix The folder directory is organized

--- Frontend-LookStyle/app/auth/login-barberia/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import LoginBarbershop from '@/components/Forms/LoginBarbershop'
-export default function LoginBarberia() {
-  return (
-    <LoginBarbershop />
-  )
-}
---
+++ Frontend-LookStyle/app/auth/loginUser/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginFormUser from "@/components/Forms/LoginFormUser";
+
+const page = () => {
+  return (
+    <>
+      <LoginFormUser />
+    </>
+  );
+};
+
+export default page;
