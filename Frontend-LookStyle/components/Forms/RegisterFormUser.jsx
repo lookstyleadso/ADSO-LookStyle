ce20e49ac9309f8a1eb765eb9e5c91800a6bed55
Kevin <kevttv29@gmail.com>
2023-12-06 21:55:40 -0500

    fix merge

--- Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
+++ Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
@@ -14,7 +14,7 @@ function UserRegister() {
   const [phone, setPhone] = useState("");
 
 
-
+}
 
   const agregarUsuario = async (nombre, lastname, correo, password, phone) => {
     let response = await fetch("https://adso-lookstyle.onrender.com/api/v1/users/", {
@@ -40,32 +40,25 @@ function UserRegister() {
   };
 
 
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
-=======
-export default function RegisterFormUser() {
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
-  return (
-    <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
-      <div className="container max-w-screen-lg mx-auto">
-        <div>
-          <h2 className="font-semibold text-xl text-gray-600">LookStyle</h2>
-          <p className="text-gray-500 mb-6">Bienvenido a LookStyle, ingresa tus datos </p>
+  export default function RegisterFormUser() {
+    return (
+      <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+        <div className="container max-w-screen-lg mx-auto">
+          <div>
+            <h2 className="font-semibold text-xl text-gray-600">LookStyle</h2>
+            <p className="text-gray-500 mb-6">Bienvenido a LookStyle, ingresa tus datos </p>
 
-          <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-              <div className="text-gray-600">
-                <p className="font-medium text-lg">Informacion Personal</p>
-                <p>Porfavor Ingresa Tus Datos.</p>
-              </div>
+            <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                <div className="text-gray-600">
+                  <p className="font-medium text-lg">Informacion Personal</p>
+                  <p>Porfavor Ingresa Tus Datos.</p>
+                </div>
 
 
 
-              <div className="lg:col-span-2">
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
-                <form onSubmit={controladorDelEnvio} >
+                <div className="lg:col-span-2">
                   <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-
-
                     <div className="md:col-span-5">
                       <label htmlFor="full_name">Name</label>
                       <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
@@ -73,92 +66,11 @@ export default function RegisterFormUser() {
                     <div className="md:col-span-5">
                       <label htmlFor="full_name">Last Name</label>
                       <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
-=======
-                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                  <div className="md:col-span-5">
-                    <label htmlFor="full_name">Name</label>
-                    <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
-                  </div>
-                  <div className="md:col-span-5">
-                    <label htmlFor="full_name">Last Name</label>
-                    <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
-                  </div>
-
-                  <div className="md:col-span-5">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="email@domain.com" />
-                  </div>
-
-                  <div className="md:col-span-5">
-                    <label htmlFor="email">Password</label>
-                    <input type="password" name="password" id="password" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="password" />
-                  </div>
-
-                  <div className="md:col-span-3">
-                    <label htmlFor="address">Address / Street</label>
-                    <input type="text" name="address" id="address" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="cra 25 # 41-40" />
-                  </div>
-
-                  <div className="md:col-span-2">
-                    <label htmlFor="city">City</label>
-                    <input type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="Cuidad" />
-                  </div>
-
-                  <div className="md:col-span-2">
-                    <label htmlFor="country">Country / region</label>
-                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                      <input name="country" id="country" placeholder="Country" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
-                      <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
-                        <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                          <line x1="18" y1="6" x2="6" y2="18"></line>
-                          <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                      </button>
-                      <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
-                        <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className="md:col-span-2">
-                    <label htmlFor="state">State / province</label>
-                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                      <input name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
-                      <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
-                        <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                          <line x1="18" y1="6" x2="6" y2="18"></line>
-                          <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                      </button>
-                      <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
-                        <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
-                      </button>
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
                     </div>
 
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
                     <div className="md:col-span-5">
                       <label htmlFor="email">Email</label>
                       <input type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="email@domain.com" />
-=======
-                  <div className="md:col-span-1">
-                    <label htmlFor="zipcode">Zipcode</label>
-                    <input type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
-                  </div>
-
-
-                  {/* Agregar los campos adicionales aquí */}
-
-                  {/* Ejemplo de campo adicional */}
-                  <div className="md:col-span-5">
-                    <label htmlFor="additional_field">Informacion Adicional</label>
-                    <input type="text" name="additional_field" id="additional_field" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
-                  </div>
-
-                  <div className="md:col-span-5 text-right">
-                    <div className="inline-flex items-end">
-                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Registrarme</button>
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
                     </div>
 
                     <div className="md:col-span-5">
@@ -206,56 +118,108 @@ export default function RegisterFormUser() {
                           <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
                         </button>
                       </div>
-                    </div>
 
-                    <div className="md:col-span-1">
-                      <label htmlFor="zipcode">Zipcode</label>
-                      <input type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
-                    </div>
-
-
-                    {/* Agregar los campos adicionales aquí */}
-
-                    {/* Ejemplo de campo adicional */}
-                    <div className="md:col-span-5">
-                      <label htmlFor="additional_field">Informacion Adicional</label>
-                      <input type="text" name="additional_field" id="additional_field" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
-                    </div>
-
-                    <div className="md:col-span-5 text-right">
-                      <div className="inline-flex items-end">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Registrarme</button>
+                      <div className="md:col-span-1">
+                        <label htmlFor="zipcode">Zipcode</label>
+                        <input type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                       </div>
+
+
+                      {/* Agregar los campos adicionales aquí */}
+
+                      {/* Ejemplo de campo adicional */}
+                      <div className="md:col-span-5">
+                        <label htmlFor="additional_field">Informacion Adicional</label>
+                        <input type="text" name="additional_field" id="additional_field" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
+                      </div>
+
+                      <div className="md:col-span-5 text-right">
+                        <div className="inline-flex items-end">
+                          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Registrarme</button>
+                        </div>
+
+                        <div className="md:col-span-5">
+                          <label htmlFor="email">Password</label>
+                          <input type="password" name="password" id="password" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="password" />
+                        </div>
+
+                        <div className="md:col-span-3">
+                          <label htmlFor="address">Address / Street</label>
+                          <input type="text" name="address" id="address" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="cra 25 # 41-40" />
+                        </div>
+
+                        <div className="md:col-span-2">
+                          <label htmlFor="city">City</label>
+                          <input type="text" name="city" id="city" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="Cuidad" />
+                        </div>
+
+                        <div className="md:col-span-2">
+                          <label htmlFor="country">Country / region</label>
+                          <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                            <input name="country" id="country" placeholder="Country" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                            <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
+                              <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                              </svg>
+                            </button>
+                            <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
+                              <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="md:col-span-2">
+                          <label htmlFor="state">State / province</label>
+                          <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                            <input name="state" id="state" placeholder="State" className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" />
+                            <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
+                              <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                              </svg>
+                            </button>
+                            <button className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600">
+                              <svg className="w-4 h-4 mx-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="md:col-span-1">
+                          <label htmlFor="zipcode">Zipcode</label>
+                          <input type="text" name="zipcode" id="zipcode" className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
+                        </div>
+
+
+                        {/* Agregar los campos adicionales aquí */}
+
+                        {/* Ejemplo de campo adicional */}
+                        <div className="md:col-span-5">
+                          <label htmlFor="additional_field">Informacion Adicional</label>
+                          <input type="text" name="additional_field" id="additional_field" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
+                        </div>
+
+                        <div className="md:col-span-5 text-right">
+                          <div className="inline-flex items-end">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Registrarme</button>
+                          </div>
+                        </div>
+
+
+                      </div>
+
                     </div>
-
-
                   </div>
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
-                </form>
-
-=======
 
                 </div>
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
               </div>
-
             </div>
+
+
           </div>
         </div>
-
-
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
-
-=======
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
       </div>
-    </div>
 
+    );
+  };
 
-  );
-};
-<<<<<<< HEAD:Frontend-LookStyle/components/Forms/UserRegister.jsx
-
-export default UserRegister;
-=======
->>>>>>> e6000ba7382e8f6a0b4ac334f6c865201219a253:Frontend-LookStyle/components/Forms/RegisterFormUser.jsx
